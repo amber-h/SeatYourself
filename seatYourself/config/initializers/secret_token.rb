9e77f534f9ace32615e5ceb428a1aa65864ffaea
amber-h <amber.houle3@gmail.com>
2013-09-18 15:38:56 -0400

    first commit

---
+++ seatYourself/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeatYourself::Application.config.secret_token = 'afacd20f9170c32220eac94ccf6d2f88076311ba99022bcaaef240133aa19fd61b4ac81b2b75b4b44d2e68113602b2b525f83eac9121e3340ee88ca68210db08'
