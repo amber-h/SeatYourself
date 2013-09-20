77ad3656a982187de5670be95c73e4314ed533b7
amber-h <amber.houle3@gmail.com>
2013-09-20 11:57:58 -0400

    added user model

--- seatYourself/app/models/reservation.rb
+++ seatYourself/app/models/reservation.rb
@@ -1,3 +1,7 @@
 class Reservation < ActiveRecord::Base
-  attr_accessible :date, :partySize, :time
+  attr_accessible :date, :partySize, :time, :user_id, :rest_id
+
+  belongs_to :restaurant
+  belongs_to :user
+
 end
