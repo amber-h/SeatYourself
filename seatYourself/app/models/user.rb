cebcb7cac5d93405084c68eeb79eb63256b4fed7
amber-h <amber.houle3@gmail.com>
2013-09-20 22:44:37 -0400

    added CanCan gem to Gemfile and added ability model

--- seatYourself/app/models/user.rb
+++ seatYourself/app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :phone_number
 
   has_secure_password
-  
+
+  ROLES = %w[owner customer]
+
   has_many :reservations
   has_many :restaurants
 
