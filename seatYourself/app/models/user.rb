5b191e4bb2eaf32634eb8c4b2dd80715df93bbfc
amber-h <amber.houle3@gmail.com>
2013-09-20 22:48:53 -0400

    adding role column to user db, migrating db

--- seatYourself/app/models/user.rb
+++ seatYourself/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :phone_number
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :phone_number, :role
 
   has_secure_password
 
--- seatYourself/db/schema.rb
+++ seatYourself/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130920155718) do
+ActiveRecord::Schema.define(:version => 20130921024519) do
 
   create_table "reservations", :force => true do |t|
     t.string   "date"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20130920155718) do
     t.string   "phone_number"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "role"
   end
 
 end
